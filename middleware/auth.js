0f6a32644ab1720e951e2d6d28f3db95c3f28832
useradmin <amutawa@hotmail.com>
2024-10-28 11:02:57 +0300

    appointment_page

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,7 @@
-const User = require('../models/User');
-
-// Middleware to check if the user is an admin
 module.exports = (req, res, next) => {
   if (req.session && req.session.userId) {
-    User.findById(req.session.userId).then(user => {
-      if (user && user.role === 'admin') {
-        next(); // Proceed if user is admin
-      } else {
-        res.status(403).send('Access denied'); // Forbidden if not admin
-      }
-    }).catch(err => {
-      res.status(500).send('Server error');
-    });
+    next();
   } else {
-    res.redirect('/users/login'); // Redirect to login if not authenticated
+    res.redirect('/users/login');
   }
 };
