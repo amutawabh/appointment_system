3e062c68b1ebbaf9888021db6d8f427e53ac2e72
useradmin <amutawa@hotmail.com>
2024-10-28 10:37:39 +0300

    passwrdadmin

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,18 @@
+const User = require('../models/User');
+
+// Middleware to check if the user is an admin
 module.exports = (req, res, next) => {
-    if (req.session && req.session.userId) {
-      next();
-    } else {
-      res.redirect('/users/login');
-    }
-  };
-  
+  if (req.session && req.session.userId) {
+    User.findById(req.session.userId).then(user => {
+      if (user && user.role === 'admin') {
+        next(); // Proceed if user is admin
+      } else {
+        res.status(403).send('Access denied'); // Forbidden if not admin
+      }
+    }).catch(err => {
+      res.status(500).send('Server error');
+    });
+  } else {
+    res.redirect('/users/login'); // Redirect to login if not authenticated
+  }
+};
